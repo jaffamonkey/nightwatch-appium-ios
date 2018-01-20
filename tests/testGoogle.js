8419eef1485c1aa1b5b6ad87bdcf2c3a2a833946
jaffamonkey <paullittlebury@gmail.com>
2018-01-20 23:15:28 +0000

    updates

--- tests/testGoogle.js
+++ tests/testGoogle.js
@@ -6,11 +6,10 @@ module.exports = {
             .assert.title('Google')
             .assert.visible('input[type=text]')
             .setValue('input[type=text]', 'rembrandt van rijn')
-            //.waitForElementVisible('.lsb', 2000)
-            ////.click('.lsb')
-            //.waitForElementVisible('input[name=btnK]', 1000)
+            .click('body')
+            .waitForElementVisible('input[name=btnK]', 1000)
             .click('input[name=btnK]')
-            .pause(2000)
+            .waitForElementVisible('#ires', 1000)
             .assert.containsText('#ires','Rembrandt - Wikipedia')
             .end();
     }
